6c3038cc5c24ef17665238cd2677738abd58021c
Sebastian Serrano <sebastian.serrano@gmail.com>
2015-02-16 10:33:20 -0800

    fix send

---
+++ setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+setup(
+  name = 'bitgo',
+  packages = ['bitgo'],
+  version = '0.1',
+  description = 'alpha version of a bitgo python library',
+  author = 'Sebastian Serrano',
+  author_email = 'sebastian@bitpagos.com',
+  url = 'https://github.com/sserrano44/pybitgo',
+  download_url = 'https://github.com/sserrano44/pybitgo/archive/master.tar.gz',
+  keywords = ['bitcoin', 'bitgo'], # arbitrary keywords
+  classifiers = [],
+)
+
